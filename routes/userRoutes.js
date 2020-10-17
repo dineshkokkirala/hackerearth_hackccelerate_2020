bec7c2305ccce3a058659f7a3c04be864475f4a9
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-10-17 08:17:13 +0530

    Login completed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,12 +3,20 @@ import mongoose from 'mongoose'
 import User from '../models/userModel.js'
 import {validationResult,check} from "express-validator";
 import bcrypt from "bcryptjs"
+import jwt from 'jsonwebtoken'
 
 const router = express.Router();
+
+
+
+
+//Register  route
+// POST     /api/user/register
+// access   Public
 router.post("/register",[
     check("username","Enter your username").not().isEmpty(),
     check("email","Email must be a valid email").isEmail(),
-    check("passowrd","Password must be 6 characters").isLength({min:6})
+    check("password","Password must be 6 characters").isLength({min:6})
 ],async(req,res)=>{
 
         const errors = validationResult(req);
@@ -33,7 +41,18 @@ router.post("/register",[
         user = new User(data);
        
         await user.save();
-        return res.status(200).json({"msg":"Registered Successfully"})
+        //return res.status(200).json({"msg":"Registered Successfully"})
+        const payload = {
+            user:{
+                id:user._id,
+                username:user.username
+            }
+        }
+
+        jwt.sign(payload,process.env.jwt_token,{expiresIn:36000},(err,token)=>{
+            if(err) throw err
+            return res.json({token})
+        })
 
 
     } catch (err) {
@@ -41,5 +60,48 @@ router.post("/register",[
         return res.status(500).json({"msg":"Internal Server Error"});
     }
 })
+
+
+//Login Route
+// POST     /api/user/login
+// access   public
+
+router.post("/login",[
+    check("email","Enter a valid email").isEmail(),
+    check("password","Password field is required").not().isEmpty()
+],async(req,res)=>{
+
+    const errors=validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    const {email,password}=req.body;
+
+    try {
+        let user=await User.findOne({email});
+
+        if(!user){
+            return res.status(422).json({msg:"Invalid Credentials"});
+        }
+
+        const isMatch=await bcrypt.compare(password,user.password)
+
+        if(!isMatch){
+            return res.status(422).json({msg:"Invalid Credentials"});
+
+        }
+
+        jwt.sign({_id:user._id,username:user.username},process.env.jwt_token,{expiresIn:36000},(err,token)=>{
+            if(err) throw err;
+            res.json({token})
+        })
+    } catch (err) {
+        console.log(err.message);
+        return res.status(500).send("Internal Server Error");
+    }
+
+})
+
 
 export default router
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app=express();
 
@@ -9,7 +10,8 @@ dotenv.config();
 //DB connection
 connectDB();
 
-
+app.use(express.json());
+app.use("/api/user/",userRoutes);
 
 app.get('/',(req,res)=>{
     res.send(`KAND........`);
